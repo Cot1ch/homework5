13603676cc807f2bca0d3225450f2b1902c20eb1
Cot1ch <vsevolod310306@mail.ru>
2024-11-28 11:35:42 +0300

    Добавьте файлы проекта.

---
+++ Tumakov5/Program.cs
@@ -0,0 +1,382 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+
+
+namespace Tumakov5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1(args);
+            Task2();
+            Task3();
+            Task4(args);
+            Task5();
+            Task6();
+
+            Console.WriteLine("Press F...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Считывает строку символов с консоли и преобразует ее к целому числу. Ввод продолжается до тех пор, 
+        /// пока пользователь не введет число.
+        /// </summary>
+        /// <returns>Число типа int</returns>
+        static int EnterNumber()
+        {
+            bool flag = true;
+            int number;
+            do
+            {
+                Console.WriteLine("Введите целое число:");
+                bool isNumber = int.TryParse(Console.ReadLine(), out number);
+                if (isNumber)
+                {
+                    flag = false;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный ввод - необходимо ввести целое число");
+                }
+            }
+            while (flag);
+
+            return number;
+        }
+
+        static int EnterMatrixSize()
+        {
+            bool flag = true;
+            int number = 0;
+            do
+            {
+                try
+                {
+                    bool isNumber = int.TryParse(Console.ReadLine(), out number);
+                    if (isNumber && number> 0)
+                    {
+                        flag = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неверный ввод - необходимо ввести целое положительное число, не превышающее 255");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            while (flag);
+
+            return number;
+        }
+
+        static void Task1(string[] args1)
+        {
+            Console.WriteLine("Упражнение 6.1");
+            if (args1 == null)
+            {
+                Console.WriteLine("Введите названия файлов через запятую БЕЗ ПРОБЕЛОВ");
+                args1 = Console.ReadLine().Split(',');
+            }
+            foreach (string arg in args1)
+            {
+                string[] paths = Directory.GetCurrentDirectory().Split('\\');
+                string path = String.Empty;
+
+                for (int i = 0; i < paths.Length - 3; i++)
+                {
+                    path += paths[i] + "/";
+                }
+
+                
+                char[] text = OpenAndReadF($"{path}/resourses/{arg}").ToCharArray();
+                Console.WriteLine($"Файл {arg}:");
+                
+                CountLettaz(text);
+            }
+        }
+
+        static void Task2()
+        {
+            Console.WriteLine("\nУпражнение 6.2");
+            Console.WriteLine("Введите количество строк первой матрицы");
+            int lenFirst = EnterMatrixSize();
+
+            Console.WriteLine("Введите количество столбцов первой (и количество строк второй) матрицы");
+            int widFirst = EnterMatrixSize();
+
+            Console.WriteLine("Введите количество столбцов второй матрицы");
+            int widSecond = EnterMatrixSize();
+
+            int[,] firstMatrix = new int[lenFirst, widFirst];
+            int[,] secondMatrix = new int[widFirst, widSecond];
+
+            Console.WriteLine("Вводим первую матрицу");
+            for (int i = 0; i < lenFirst; i++)
+            {
+                Console.WriteLine("Новая строка");
+                for (int j = 0; j < widFirst; j++)
+                {
+                    firstMatrix[i, j] = EnterNumber();
+                }
+            }
+            PrintMatrix(firstMatrix);
+
+            Console.WriteLine("Вводим вторую матрицу");
+            for (int i = 0; i < widFirst; i++)
+            {
+                Console.WriteLine("Новая строка");
+                for (int j = 0; j < widSecond; j++)
+                {
+                    secondMatrix[i, j] = EnterNumber();
+                }
+            }
+            PrintMatrix(secondMatrix);
+
+            Console.WriteLine("Результат умножения равен:");
+            PrintMatrix(MulMatrixs(firstMatrix, secondMatrix));
+        }
+
+        static int[,] MulMatrixs(int[,] fMatrix, int[,] sMatrix)
+        {
+            int n = fMatrix.GetLength(0);
+            int m = fMatrix.GetLength(1);
+            int l = sMatrix.GetLength(1);
+
+            int[,] answer = new int[n, l];
+            int dop = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < l; j++)
+                {
+                    for (int k = 0; k < m; k++)
+                    {
+                        dop += fMatrix[i, k] * sMatrix[k, j];
+                    }
+                    answer[i, j] = dop;
+                    dop = 0;
+                }
+            }
+
+            return answer;
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+            Console.WriteLine("Матрица:");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Task3()
+        {
+            Console.WriteLine("\nУпражнение 6.3");
+            int[,] temperature = new int[12, 30];
+            string[] months = { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" };
+
+            Random rnd = new Random();
+
+            Console.WriteLine("Средняя температура по месяцам");
+            for (int i = 0; i < temperature.GetLength(0); i++)
+            {
+                Console.WriteLine(months[i]);
+                for (int j = 0; j < temperature.GetLength(1); j++)
+                {
+                    temperature[i, j] = rnd.Next(-30, 30);
+                    Console.Write($"{temperature[i, j],3}|");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Средние температуры:");
+            double[] ans = GetMeanTemp(temperature);
+
+            for (int i = 0; i < ans.Length; i++) 
+            {
+                Console.Write($"{ans[i]:F1}{"\u00B0"}C ");
+            }
+        }
+
+        static double[] GetMeanTemp(int[,] arr)
+        {
+            double[] meanTemp = new double[12];
+
+            for (int month = 0; month < 12; month++)
+            {
+                int mean = 0;
+                for (int day = 0; day < 30; day++)
+                {
+                    mean += arr[month, day];
+                }
+                meanTemp[month] = mean / 12.0;
+            }
+            Array.Sort(meanTemp);
+            return meanTemp;
+        }
+
+        static void Task4(string[] args4)
+        {
+            Console.WriteLine("\nДомашнее задание 6.1");
+            foreach (string arg in args4)
+            {
+                string[] paths = Directory.GetCurrentDirectory().Split('\\');
+                string path = String.Empty;
+
+                for (int i = 0; i < paths.Length - 3; i++)
+                {
+                    path += paths[i] + "/";
+                }
+
+                List<char> text = new List<char>();
+
+                foreach (char letter in OpenAndReadF($"{path}/resourses/{arg}"))
+                {
+                    text.Add(char.ToLower(letter));
+                }
+                Console.WriteLine($"Файл {arg}:");
+                CountLettaz(text);
+            }
+        }
+
+        static void CountLettaz(char[] letters)
+        {
+            long vowelsCount = 0;
+            long consonantsCount = 0;
+            foreach (char letter in letters)
+            {
+                if ("eyuioaуеыаоёяию".Contains(letter))
+                {
+                    vowelsCount++;
+                }
+                else if ("йцкнгшщзхфвпрлджчсмтъьбqwrtpsdfghjklzxcvbnm".Contains(letter))
+                {
+                    consonantsCount++;
+                }
+            }
+            
+            Console.WriteLine($"Количество гласных = {vowelsCount}, согласных = {consonantsCount}\n");
+        }
+        static void CountLettaz(List<char> letters)
+        {
+            long vowelsCount = 0;
+            long consonantsCount = 0;
+            foreach (char letter in letters)
+            {
+                if ("eyuioaуеыаоёяию".Contains(letter))
+                {
+                    vowelsCount++;
+                }
+                else if ("йцкнгшщзхфвпрлджчсмтъьбqwrtpsdfghjklzxcvbnm".Contains(letter))
+                {
+                    consonantsCount++;
+                }
+            }
+
+            Console.WriteLine($"Количество гласных = {vowelsCount}, согласных = {consonantsCount}");
+        }
+
+        static string OpenAndReadF(string filePath)
+        {
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    string readContent = File.ReadAllText(filePath);
+                    return readContent;
+                }
+                else
+                {
+                    throw new Exception("Файл не найден");
+                }
+            }
+            catch (Exception ex) 
+            { 
+                Console.WriteLine(ex.Message);
+            }
+            return null;
+        }
+
+        static void Task5() ///Дописать!!!!!!!!!!!
+        {
+            Console.WriteLine("\nДомашнее задание 6.2");
+
+            Console.WriteLine("Введите количество строк первой матрицы");
+            int lenFirst = EnterMatrixSize();
+
+            Console.WriteLine("Введите количество столбцов первой (и количество строк второй) матрицы");
+            int widFirst = EnterMatrixSize();
+
+            Console.WriteLine("Введите количество столбцов второй матрицы");
+            int widSecond = EnterMatrixSize();
+
+            LinkedList<LinkedList<int>> firstMatrix = new LinkedList<LinkedList<int>>();    
+            LinkedList<LinkedList<int>> secondMatrix = new LinkedList<LinkedList<int>>();
+
+            LinkedList<int> line = new LinkedList<int>();
+
+            for (int i = 0; i < lenFirst; i++)
+            {
+                for (int j = 0; j < widFirst; j++)
+                {
+                    line.AddLast(EnterNumber());
+                }
+                firstMatrix.AddLast(line);
+            }
+            Console.WriteLine(secondMatrix);
+
+            for (int i = 0; i < widFirst; i++)
+            {
+                for (int j = 0; j < widSecond; j++)
+                {
+                    line.AddLast(EnterNumber());
+                }
+                firstMatrix.AddLast(line);
+            }
+            Console.WriteLine(secondMatrix);
+        }
+
+        static void Task6()
+        {
+            Console.WriteLine("\nДомашнее задание 6.3");
+
+            Random rnd = new Random();
+            Dictionary<string, int[]> temperature = new Dictionary<string, int[]>();
+            string[] months = { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" };
+            
+            for (int i = 0; i < months.Length; i++)
+            {
+                int[] days = new int[30];
+                Console.WriteLine(months[i]);
+
+                for (int j = 0; j < 30;  j++)
+                {
+                    days[j] = rnd.Next(-30, 30);
+                    Console.Write($"{days[j], 3}|");
+                }
+                Console.WriteLine();
+                temperature[months[i]] = days;
+            }
+
+            Console.WriteLine("Средние температуры: ");
+            foreach (string month in months)
+            {
+                Console.WriteLine($"{month}: {temperature[month].Sum()/12.0:F1}{"\u00B0"}C");
+            }
+        }
+
+    }
+}
---
+++ homework5/Program.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace homework5
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Task1();
+            Task2();
+            Task3();
+            Task4();
+
+            Console.WriteLine("Press something...");
+            Console.ReadKey();
+
+        }
+        static void Task1()
+        {
+            
+        }
+        static void Task2()
+        {
+            Dictionary<string, object[]> students = new Dictionary<string, object[]>();
+            students["Целоусов_Игорь"] = new object[] { 2005, "физика", 244 };
+            students["Осипов_Семён"] = new object[] { 2005, "информатика", 252 };
+            students["Кузьмина_Анастасия"] = new object[] { 2006, "информатика", 246 };
+            students["Гомза_Арина"] = new object[] { 2006, "информатика", 250 };
+            students["Харламова_Анна"] = new object[] { 2006, "английский", 249 };
+            students["Квятковский_Всеволод"] = new object[] { 2006, "физика", 301 };
+            students["Закиров_Айназ"] = new object[] { 2006, "информатика", 252 };
+            students["Боронин_Никита"] = new object[] { 2006, "физика", 247 };
+            students["Садриев_Салават"] = new object[] { 2006, "информатика", 240 };
+            students["Калимуллин_Алмаз"] = new object[] { 2006, "информатика", 245 };
+
+            bool flag = true;
+            do
+            {
+                Console.WriteLine("Если вы хотите добавить студента, введите 'новый студент' в любом регистре без кавычек");
+                Console.WriteLine("Если вы хотите удалить студента, введите 'удалить' в любом регистре без кавычек");
+                Console.WriteLine("Если вы хотите вывести отсортированный список студентов, введите 'сортировать' в любом регистре без кавычек");
+                Console.WriteLine("Чтобы завершить сеанс, нажмите Enter");
+
+                string input = Console.ReadLine();
+                if (String.IsNullOrEmpty(input))
+                {
+                    flag = false;
+                }
+                else
+                {
+                    switch (input.ToLower())
+                    {
+                        case "новый студент":
+                            object[] newStud = new object[5];
+                            newStud = NewStudent();
+
+                            string key = newStud[1].ToString() + "_" + newStud[0].ToString();
+                            object[] value = new object[3];
+                            for (int i = 0; i < newStud.Length - 2; i++)
+                            {
+                                value[i] = newStud[i + 2];
+                            }
+
+                            students[key] = value;
+                            Console.WriteLine("Студент добавлен");
+                            break;
+                        case "удалить":
+                            DelStudent(students);
+                            break;
+                        case "сортировать":
+                            SortStud(students);
+                            break;
+                        default:
+                            Console.WriteLine("Почти, но не то. Попробуйте снова. Рахмэт");
+                            break;
+                    }
+                }
+            }
+            while (flag);
+        }
+        static int EnterPosNumber()
+        {
+            bool flag = true;
+            int number;
+            do
+            {
+                bool isNumber = int.TryParse(Console.ReadLine(), out number);
+                if (isNumber)
+                {
+                    flag = false;
+                }
+                else if (number <= 0)
+                {
+                    Console.WriteLine("Ну это прям хорошо меня вынесло. Но нет");
+                }
+                else
+                {
+                    Console.WriteLine("Неверный ввод - необходимо ввести целое число");
+                }
+            }
+            while (flag);
+
+            return number;
+        }
+        static int EnterBirthYear()
+        {
+            bool flag = true;
+            int number;
+            do
+            {
+                Console.WriteLine("Введите год рождения:");
+                bool isNumber = int.TryParse(Console.ReadLine(), out number);
+                if (isNumber)
+                {
+                    flag = false;
+                }
+                else if (number <= 0)
+                {
+                    Console.WriteLine("Ну ок");
+                    flag = false;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный ввод - необходимо ввести целое число");
+                }
+            }
+            while (flag);
+
+            return number;
+        }
+        static object[] NewStudent()
+        {
+            Console.WriteLine("Введите имя");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Введите фамилию");
+            string lastName = Console.ReadLine();
+
+            int birthYear = EnterBirthYear();
+            Console.WriteLine("Введите экзамен");
+            string exam = Console.ReadLine();
+            Console.WriteLine("Введите итоговую сумму баллов");
+            int scores = EnterPosNumber();
+
+            object[] ret = new object[] { firstName, lastName, birthYear, exam, scores };
+            return ret;
+        }
+        static void DelStudent(Dictionary<string, object[]> dict)
+        {
+            Console.WriteLine("Введите имя");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Введите фамилию");
+            string lastName = Console.ReadLine();
+            dict.Remove(lastName + "_" + firstName);
+
+            Console.WriteLine($"{firstName} {lastName} благополучно удалён");
+        }
+
+        static void SortStud(Dictionary<string, object[]> dict)
+        {
+            Console.WriteLine("Отсортированный список:");
+            foreach (var info in dict.OrderBy(info => info.Value[2]))
+            {
+                foreach (string str in info.Key.Split('_'))
+                {
+                    Console.Write(str + " ");
+                }
+                Console.Write($"\nГод рождения: {info.Value[0]}; ");
+                Console.Write($"Экзамен: {info.Value[1] + ";",-12} ");
+                Console.Write($"Итоговая сумма баллов: {info.Value[2]}\n");
+            }
+        }
+
+        static void Task3()
+        {
+            Queue<Grandma> grandmas = new Queue<Grandma>();
+            Stack<Hospital> hospitals = new Stack<Hospital>();
+
+            hospitals.Push(new Hospital()
+            {
+                name = "Городская поликлиника",
+                capacity = 5,
+                deseases = new List<string> { "грипп", "простуда" }
+            });
+            hospitals.Push(new Hospital()
+            {
+                name = "Негородская поликлиника",
+                capacity = 2,
+                deseases = new List<string> { "кашель", "простуда" }
+            });
+            hospitals.Push(new Hospital()
+            {
+                name = "Максимально странная поликлиника",
+                capacity = 3,
+                deseases = new List<string> { "забыл, как называется", "склероз", "ещё что-то" }
+            });
+
+            //Ввод бабушек 
+            bool flag = true;
+            do
+            {
+                Console.WriteLine("Введите имя бабушки [либо 'выход' для выхода (логично)]");
+                string input = Console.ReadLine();
+                if (input.ToLower() == "выход")
+                {
+                    flag = false;
+                }
+                else
+                {
+                    Console.WriteLine("Введите возраст бабушки");
+                    int ageInt = EnterPosNumber();
+
+                    Console.WriteLine("Введите болезни бабушки. Если их несколько, используйте запятую как разделитель");
+                    string[] desStr = Console.ReadLine().Split(',');
+
+                    Console.WriteLine("Введите лекарства бабушки. Если их несколько, используйте запятую как разделитель");
+                    string[] medStr = Console.ReadLine().Split(',');
+
+                    Grandma newGrandma = new Grandma()
+                    {
+                        name = input,
+                        age = ageInt,
+                        deseases = desStr.ToList(),
+                        medicines = medStr.ToList()
+                    };
+
+                    grandmas.Enqueue(newGrandma);
+                }
+            }
+            while (flag);
+
+
+            int countCryGrandmas = 0;
+            Grandma grandma = new Grandma();
+            do
+            {
+                grandma = grandmas.Dequeue();
+
+                if (String.IsNullOrEmpty(grandma.deseases[0]))
+                {
+                    Console.WriteLine(grandma.deseases);
+                
+                    foreach (var hospital in hospitals)
+                    {
+                        int des = grandma.deseases.Count(desease => hospital.deseases.Contains(desease));
+                        
+                        if ((double)des >= 0.5)
+                        {
+                            Console.WriteLine($"Бабушкино лечение продолжится в {hospital.name}");
+                            flag = false;
+                            grandma.status = $"{hospital.name}";
+                            break;
+                        }
+                    }
+                    if (flag)
+                    {
+                        Console.WriteLine("Бабушка отправляется плакать((");
+                        countCryGrandmas++;
+                    }
+
+                }
+                else
+                {
+                    while (hospitals.Count > 0)
+                    {
+                        Hospital hosp = hospitals.Pop();
+                        if (hosp.waitList < hosp.capacity)
+                        {
+                            hosp.waitList++;
+                            Console.WriteLine(hosp.waitList);
+                            break;
+                        }
+                    }
+                }
+
+            }
+            while (grandmas.Count > 0);
+
+            foreach (Hospital hospital in hospitals)
+            {
+                Console.WriteLine(hospital.waitList);
+            }
+            
+        }
+        
+        static void Task4()
+        {
+
+        }
+    }
+    struct Grandma
+    {
+        public string name;
+        public int age;
+        public List<string> deseases;
+        public List<string> medicines;
+        public string status;
+    }
+    struct Hospital
+    { 
+        public string name;
+        public List<string> deseases;
+        public int capacity;
+        public int waitList;
+    }
+}
